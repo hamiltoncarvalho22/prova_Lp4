7812fcb65a7e3d999585fad8712a1053c7f65ad5
hamiltoncarvalho22 <55262517+hamiltoncarvalho22@users.noreply.github.com>
2021-12-13 23:54:00 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Titulo from './Titulo';
+import Texto from './Texto';
+
+
+
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <Titulo />
+        <Texto />
+        </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Texto.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Texto extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Texto</h1>
+                <p>PROVA MUITO BOA PROFESSOR!!!</p>
+            </div>
+        );
+    }
+}
+
+export default Texto;
